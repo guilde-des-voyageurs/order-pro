504acad697422a15ebce34f850b70b9ad643dc58
Uriel <uriel@lahoussaye.fr>
2026-01-25 16:39:06 +0100

    flexbox menu

--- src/app/ivy/layout.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { IvyLayout } from '@/layout/IvyLayout';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <IvyLayout>{children}</IvyLayout>;
-}
